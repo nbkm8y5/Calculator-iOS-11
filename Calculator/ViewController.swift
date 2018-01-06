6c80cd1c991724c171a0ce4a72467426505ac76b
Rolando Moreno <moreno.ronline@gmail.com>
2018-01-06 12:52:59 -0500

    First Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rolando Moreno on 12/28/17.
+//  Copyright © 2017 Greasy Hacks, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+}
+
